e82135a6db5b9ec0d0488274f219a7e27abf81e6
Sandra Petereit <sandrap3393@googlemail.com>
2022-11-23 16:43:46 +0100

    add counter and lightswitch kata

---
+++ counter/script.js
@@ -0,0 +1,8 @@
+let counter = 0;
+
+const countArea = document.querySelector("#counter");
+
+countArea.addEventListener("click", function () {});
+
+//global scope for key pressing
+document.addEventListener("keyup", function (e) {});
---
+++ light-switch/script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector(".button");
+const body = document.querySelector("body");
+
+btn.addEventListener("click", function () {
+  btn.classList.toggle("button--dark");
+  body.classList.toggle("body--dark");
+  //console.log("switch");
+});
