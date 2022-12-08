e8631c32c93063d54498434751de48a5229554b4
Sandra Petereit <sandrap3393@googlemail.com>
2022-12-08 17:54:55 +0100

    add fast-cheap-good kata

---
+++ fast-cheap-good/script.js
@@ -0,0 +1,35 @@
+const checkboxFast = document.getElementById("checkFast");
+const checkboxCheap = document.getElementById("checkCheap");
+const checkboxGood = document.getElementById("checkGood");
+
+const checkboxes = [checkboxFast, checkboxCheap, checkboxGood];
+let lastChecked = null;
+
+for (const element of checkboxes) {
+  element.addEventListener("change", (e) => {
+    /**
+     * prevent all checkboxes being checked at the same time
+     * if a third checkbox is checked, uncheck box which was checked before
+     */
+    if (getCheckboxCheckedCount() >= checkboxes.length) {
+      if (lastChecked !== e.target) {
+        lastChecked.checked = false;
+      }
+    }
+    lastChecked = e.target;
+  });
+}
+
+/**
+ * count checked checkboxes
+ * @returns number of total checked input elements
+ */
+function getCheckboxCheckedCount() {
+  let count = 0;
+  checkboxes.forEach((element) => {
+    if (element.checked) {
+      count++;
+    }
+  });
+  return count;
+}
