8c97fcdf09f6db20f08a520454154efdde311ac9
Sandra Petereit <sandrap3393@googlemail.com>
2022-12-18 21:55:47 +0100

    synchronize background with counter

--- counter/script.js
+++ counter/script.js
@@ -1,11 +1,13 @@
 let counter = 0;
 
 const clickArea = document.getElementById("click-area");
+const bar = document.getElementById("bar");
 const counterDisplay = document.getElementById("counter-label");
 
 clickArea.addEventListener("click", () => {
   counter++;
   counterDisplay.innerText = counter;
+  changeColorAtCount();
 });
 
 //use global scope for keyboard input
@@ -14,6 +16,9 @@ document.addEventListener("keyup", () => {
   counterDisplay.innerText = counter;
 });
 
+changeColorAtCount();
 /******************************************************/
 
-function changeColorAtCount() {}
+function changeColorAtCount() {
+  clickArea.style.backgroundSize = `${counter}% 100%`;
+}
